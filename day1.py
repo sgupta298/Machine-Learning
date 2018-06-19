67f10c161af6ec991cf1c5e59ce4685cf8308f08
Sanjay Gupta <40050720+sgupta298@users.noreply.github.com>
2018-06-19 16:12:50 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,22 +1,3 @@
-
-''''
-print("press 1 to show date and time")
-print("press 2 to create a file")
-print("press 3 to create a directory")
-print("press 4 to google search")
-print("press 5 to log out of system")
-print("press 6 to shut down pc")
-print("press 7 to check internet connection")
-print("press 8 to login whatsapp on browser")
-print("press 9 to check all connected ip on network")
-x=int(input())
-if x= 1 :
-
-if x= 1 :
-
-if x= 1:
-
-if x= 1:
 x=[]
 y=[]
 z=[]
@@ -40,7 +21,7 @@ x.reverse()
 print(x)
 print(x.index(3))
 y=x.copy()
-print(y)'''
+print(y)
 class students:
     def __init__(self, name, rollno):
         self.name = name
